5345c75c440c5498e0ade2565de83cd78f3f7f03
hannaleve <hannakwak00@gmail.com>
2023-05-02 17:46:23 +0900

    commit success

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -3,5 +3,7 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+/*
+ This file is intentionally blank
+ Use this file to add JavaScript to your project
+*/
